26d192bdaf79d1abbead52601c4752bd1b9f1599
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2024-02-27 21:13:23 +0100

    Add project files.

---
+++ Uppgift 7.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Uppgift_7._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sträng = new string[] { "hej", "nej", "ja" };
+            char tecken = 'j';
+
+            if (FinnsTecknetIAllaSträngar(sträng, tecken))
+            {
+                Console.WriteLine($"{tecken} finns i alla orden");
+            }
+            else
+            {
+                Console.WriteLine($"{tecken} finns inte i alla orden");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool FinnsTecknetIAllaSträngar(string[] str, char tecken)
+        {
+            foreach(var ord  in str)
+            {
+                if (!ord.Contains(tecken))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
